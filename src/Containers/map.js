0e87ecfa32aadf35614e7c869dfed4222e59178f
ravindra rawat <ravinrawat.wd@gmail.com>
2018-07-02 21:17:25 -0600

    redux

---
+++ src/Containers/map.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Map = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: #000000;
+`;
+
+class map extends Component {
+  render() {
+    return <Map />;
+  }
+}
+
+export default map;
